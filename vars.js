bb1d610f9273e4e14820784d6042e7354f92cbc8
Aryan Singh <83677526+aru24sin@users.noreply.github.com>
2025-05-02 12:45:13 -0400

    Javascript variables

---
+++ vars.js
@@ -0,0 +1,17 @@
+"use struct";
+//This code works the modern way
+
+let message;
+message = "Hello";
+
+let name = "Ary"
+let date = "5/2/2025";
+
+alert(message);
+alert(name);
+alert(date);
+alert(3 + 2);
+// Example of a comment
+/* Example
+    of a 
+    multiline comment */
